619a192ba3b122daa66f336c3474b33889a0ee01
bawa-hub <itsworkingmind@gmail.com>
2022-03-12 20:08:35 +0530

    middleware api

--- _5_.js
+++ _5_.js
@@ -4,3 +4,25 @@ import {
   applyMiddleware,
   bindActionCreators,
 } from "redux";
+
+const reducer = (state = { count: 1 }) => state;
+
+const logMiddleware = (store) => (next) => (action) => {
+  console.log("old state", store.getState(), action);
+  next(action);
+  console.log("new state", store.getState(), action);
+};
+
+const monitorMiddleware = (store) => (next) => (action) => {
+  const start = performance.now();
+  next(action);
+  const end = performance.now();
+  const diff = end - start;
+  console.log(diff);
+};
+const store = createStore(
+  reducer,
+  applyMiddleware(logMiddleware, monitorMiddleware)
+);
+
+store.dispatch({ type: "hello" });
